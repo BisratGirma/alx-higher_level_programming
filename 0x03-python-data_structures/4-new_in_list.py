63763a042bf6fb9684d31cd46b58d4eb2ea37939
bisrax <bisratx#gmail.com>
2021-09-14 20:34:02 +0300

    4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,11 +5,12 @@ dont copy the address of the old list
 '''
 
 def new_in_list(my_list, idx, element):
-    new_list=list(my_list)
-
     if my_list is None:
         pass
-    elif idx < 0 and idx >= len(my_list):
+    
+    new_list=list(my_list)
+   
+    if idx < 0 and idx >= len(my_list):
         return new_list
     else:
         new_list[idx]=element
