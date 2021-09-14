3f701e1713f627768264f37df9b27ee479bf2158
bisrax <bisratx#gmail.com>
2021-09-14 20:37:43 +0300

    12 hot fix on logical operator

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -10,7 +10,7 @@ def new_in_list(my_list, idx, element):
     
     new_list=list(my_list)
    
-    if idx < 0 and idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return new_list
     else:
         new_list[idx]=element
