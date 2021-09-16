69c4a7af9816b14da47a95cdca3facb9419a6e36
bisrat <30285329+bisrax@users.noreply.github.com>
2021-09-16 06:50:41 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,5 +6,8 @@ deletes c's from string
 
 
 def no_c(my_string):
-    copy_str = [x for x in my_string if x != 'c' or x != 'C']
-    return ("".join(copy_str))
+    new_str = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str += char
+    return new_str
