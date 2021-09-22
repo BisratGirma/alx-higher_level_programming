e97cb924b3b9bf57d70984b57aeacdf98feadffc
bisrax <bisratx#gmail.com>
2021-09-22 12:29:01 +0300

    index fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
         return my_list
     new = [i for i in my_list]
     for i, v in enumerate(my_list):
-        if v == search - 1:
-            new[v] = replace
+        if v == search:
+            new[i] = replace
     return new
