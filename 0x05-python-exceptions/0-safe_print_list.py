820ec75c7403d431f7eaad77431fe6f1714158d2
bisrax <bisratx#gmail.com>
2021-09-29 11:56:53 +0300

    fix logic line

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,10 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for item in my_list:
         try:
-            print("{}".format(item), end="")
-            i += 1
             if i >= x:
                 raise
+            print("{}".format(item), end="")
+            i += 1
         except:
             break
     print("")
