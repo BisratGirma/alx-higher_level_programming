f6ed31d6b4256e40a3b121a035e2f56ef05f264e
bisrax <bisratx#gmail.com>
2021-09-29 14:04:48 +0300

    lame

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
-    for item in my_list:
+    for num in range(x):
         try:
-            if i >= x:
-                raise IndexError('list index out of range')
-            print("{:d}".format(item), end="")
+            print("{:d}".format(my_list[num]), end="")
             i += 1
-        except IndexError:
-            break
         except (ValueError, TypeError):
             continue
     print("")
