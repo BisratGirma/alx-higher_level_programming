278eb53270afae684a636c11fe8671a07bacd32d
bisrax <bisratx#gmail.com>
2021-10-02 16:53:25 +0300

    init 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A simple example of defining class.
+
+This class is empty. defined only for example
+
+"""
+
+
+class Square:
+"""This class defines object of square..
+
+"""
+    
+    def __init__(self, size=''):
+        """ defines properties of square object.
+
+        """
+
+        self.__size = size
