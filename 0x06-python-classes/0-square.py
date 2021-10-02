d83a4dbd3cbacf5baf34e8b140958f944735cbfb
bisrax <bisratx#gmail.com>
2021-10-02 14:20:41 +0300

    INIT

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A simple example of defining class.
+
+This class is empty. defined only for example
+
+"""
+
+class Square:
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+# a All about object oriented programming
+** a project done for ALX holberton programme **
