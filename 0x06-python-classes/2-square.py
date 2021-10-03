f317f5801b6c676034e643512b4d690aacac5518
bisrax <bisratx#gmail.com>
2021-10-03 19:16:03 +0300

    simple style

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """A simple example of defining class.
 
-This class is empty. defined only for example
-
 """
 
 
 class Square:
-    """This class defines object of square..
+    """This class defines object of square with size.
 
     """
     def __init__(self, size=0):
