c7aa2d90c0ac87d24978981e4320efd3bbb32c67
bisrax <bisratx#gmail.com>
2021-10-09 15:12:31 +0300

    just a fix on None type

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,24 @@
+#!/usr/bin/python3
+"""Adding Two values. """
+
+
 def add_integer(a, b=98):
+    """This functions addes two integers
+    Args:
+        a (int): first integer value.
+        b (int): second integer value.
+
+    Return:
+        The sum of the arguments
+
+    """
     if type(a) is float:
         a = int(a)
     if type(b) is float:
         b = int(b)
     b = int(b)
-    if type(a) is not int:
-        raise TypeError("{} must be an integer".format(a))
-    if type(b) is not int:
-        raise TypeError("{} must be an integer".format(b))
+    if a is None or type(a) is not int:
+        raise TypeError("a must be an integer")
+    if b is None or type(b) is not int:
+        raise TypeError("b must be an integer")
     return a + b
