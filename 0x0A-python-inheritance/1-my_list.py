5bd67b5040bacbca3582d0fa5c86804e238c5ff1
bisrax <bisratx#gmail.com>
2021-10-13 18:49:39 +0300

    2nd try

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Inheritance exercise Module. """
+
+
+class Mylist(list):
+    """
+
+    """
+
+    def print_sorted(self):
+        pass
+
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Inheritance exercise Module. """
+
+
+def is_same_class(obj, a_class):
+    """Checks if object is instance a class
+
+    Args:
+        obj (obj): object
+        a_class (class): a class to match with obj
+
+    Return:
+        True: if the object is instance of the class
+        False: if not.
+    """
+
+    return isinstance(obj, a_class)
