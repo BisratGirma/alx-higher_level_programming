98d93ea489009dc4f2868625083c1714cbedb8e0
bisrax <bisratx#gmail.com>
2021-10-17 15:56:52 +0300

    fixed type error on b

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,13 +12,12 @@ def add_integer(a, b=98):
         The sum of the arguments
 
     """
+    if a is None or (type(a) is not int and type(a) is not float):
+        raise TypeError("a must be an integer")
+    if b is None or (type(b) is not int and type(b) is not float):
+        raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
         b = int(b)
-    b = int(b)
-    if a is None or type(a) is not int:
-        raise TypeError("a must be an integer")
-    if b is None or type(b) is not int:
-        raise TypeError("b must be an integer")
     return a + b
